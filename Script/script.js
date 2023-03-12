530b2a553e4eee89f109d640b53da1388fcd7b37
Elias Al-Swais <elias.al-swais@pwc.com>
2023-03-12 16:07:18 +0300

    Added A button that will change the direction of the images using JS

--- Script/script.js
+++ Script/script.js
@@ -1,9 +1,13 @@
 function changeColor(){
-    var color = prompt("What color would you like to change the paragraph to?");
+    var color = prompt("What color would you like to change the paragraph to?")
     var test =document.getElementsByClassName("middleParagraph")
     for(var i = 0; i<test.length; i++){
         test[i].style.color = color;
     }
-    
+}
 
+function changeDirection(){
+    var direction = prompt("What direction do you want the images to be?")
+    document.getElementById("imagegallery").style.flexDirection = direction;
+    
 }
